86a8698e4a442c5bf570cab587fada2749c42573
bWyatt2026 <blake.wyatt@stu.daviess.kyschools.us>
2025-04-07 14:27:13 +0000

    ss

--- Trio.java
+++ Trio.java
@@ -1,1 +1,31 @@
 //Write your answer here
+public Trio
+{
+    Sandwhich sand = "";
+    Salad salad = "";
+    Drink drink = "";
+    public Trio(String s, String sal, String dr)
+    {
+        sand = s;
+        salad = sal;
+        drink = dr
+
+    }
+
+    public String toString()
+    {
+        return  "" + sand.getName() + "/ " + salad.getName() + "/ " + drink.getName() + " Trio.";
+    }
+    public double trioCost()
+    {
+        double price = 0;
+        if(sand.getPrice() > salad.getPrice())
+        {
+          if(sand.getPrice() > drink.getPrice())
+          {
+            price = sand.getPrice() + drink.getPrice();
+          }
+        }
+
+    }
+}
